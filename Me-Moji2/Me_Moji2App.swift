c7a3329d22eba07684f935318816706761df2b91
Brett <eplfury@yahoo.com>
2022-11-28 18:56:56 -0500

    Initial Commit

---
+++ Me-Moji2/Me_Moji2App.swift
@@ -0,0 +1,17 @@
+//
+//  Me_Moji2App.swift
+//  Me-Moji2
+//
+//  Created by Brett Tracey on 11/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Me_Moji2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
